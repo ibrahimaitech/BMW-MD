8d8df9a30a8d8a3deb6f28eae4f30ac9e20d1efe
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-03-09 23:35:03 +0300

    ☣IBRAHIM-TECH☣.js

--- set.js
+++ set.js
@@ -9,7 +9,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || 'zokk',
     PREFIXE: process.env.PREFIX || "~",
-    OWNER_NAME: process.env.OWNER_NAME || "France King",
+    OWNER_NAME: process.env.OWNER_NAME || "IBRAHIM-TECH",
     NUMERO_OWNER : process.env.NUMERO_OWNER || "254751284190",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
