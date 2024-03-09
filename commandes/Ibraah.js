d2aac618c41141455704c30e571244d54fa4645b
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-03-10 01:07:58 +0300

    ☣IBRAHIM-TECH☣

--- commandes/Ibraah.js
+++ commandes/Ibraah.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "repo", reaction: "📷", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "Ibraah", reaction: "📷", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = 'IBRAAH-HD-PHOTO \n\n ' + "*ⁱᵇʳᵃʰⁱᵐ-ᵗᵉᶜʰ.*";
     let d = ' #KING'S';
