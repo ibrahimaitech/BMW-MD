82af5ecb33a89ec2630d9e3e2408998bfef06e96
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-02 08:29:14 +0300

    Add files via upload

--- commandes/parole.js
+++ commandes/parole.js
@@ -1,8 +1,40 @@
-const {zokou} =require("../framework/zokou");
+const {france} =require("../framework/france");
 const axios =require("axios");
+const Genius = require("genius-lyrics"); 
+ const Client = new Genius.Client("jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO");
 
 
-zokou({ nomCom: "lyrics",
+
+france({ nomCom: "poll",
+        reaction: "✨",
+        categorie: "General" }, async (dest, zk, commandeOptions) => {
+    
+    const { repondre, arg, ms } = commandeOptions; 
+const polll = arg.join(' ');
+
+
+
+let [poll, opt] = polll.split("|")
+
+if (polll.split("|") < 2)
+                return repondre(`Incorrect format.\nExample: poll what is 1+1|2, 3, 4`);
+
+let options = []
+            for (let i of opt.split(',')) {
+                options.push(i)
+            }
+            await zk.sendMessage(dest, {
+                poll: {
+                    name: poll,
+                    values: options
+                }
+            })
+
+})
+
+ 
+        
+france({ nomCom: "lyrics",
         reaction: "✨",
         categorie: "Search" }, async (dest, zk, commandeOptions) => {
     
@@ -12,25 +44,19 @@ zokou({ nomCom: "lyrics",
 
     if (!arg || arg.length === 0) return repondre("please provide me the song name");
 
-    let  result  = await axios.get(`https://vihangayt.me/search/lyrics?q=${arg.join(' ')}`);
+         const question = arg.join(' ');
 
-    let lyrics = result.data.data;
+ 
+  
+ const searches = await Client.songs.search(question); 
+ const firstSong = searches[0]; 
+ const lyrics = await firstSong.lyrics(); 
+ await zk.sendMessage(dest, { text: lyrics}, { quoted: ms }); 
+ } catch (error) { 
+             reply(`I did not find any lyrics for ${text}. Try searching a different song.`); 
+             console.log(error); 
+         } 
 
-    if (lyrics.error) return repondre("no lyrics found");
-
-    let msg = `---------FLASH-MD-lyrics-finder--------
-
-* *Artist :* ${lyrics.artist}
 
 
-* *Title :* ${lyrics.title}
-
-
-${lyrics.lyrics}`
-
-    zk.sendMessage(dest,{image : { url : './media/lyrics-img.jpg'} , caption : msg}, { quoted : ms });
-    
-   } catch (err) {
-       repondre('Error')
-   }
         })
