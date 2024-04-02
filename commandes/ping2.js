d02e077cbffaac77720805a7f1b1dec07dd24928
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-02 22:14:40 +0300

    Update ping2.js

--- commandes/ping2.js
+++ commandes/ping2.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "ping", reaction: "💯", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+const { france } = require("../framework/france");
+france({ nomCom: "ping", reaction: "💯", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = '𝐏𝐈𝐍𝐆𝐈𝐍𝐆 𝐅𝐑𝐎𝐌⌛*𝗜𝗕𝗥𝗔𝗛𝗜𝗠-𝗧𝗘𝗖𝗛* \n\n ' + "LOADING⏳..✨✨✨..100℅✅";
-    let d = '𝗣𝗢𝗡𝗚:1090000000𝗠𝗦⏳*';
+    let d = '𝗣𝗢𝗡𝗚:1090𝗠𝗦⏳*';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/e3d2edb3167a59ba625d7.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
