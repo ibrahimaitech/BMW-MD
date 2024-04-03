3048da8049d6b8dc270a6c60dc156fe0f47b22cc
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-03 20:45:14 +0300

    Google

--- commandes/anime.js
+++ commandes/anime.js
@@ -145,7 +145,7 @@ france({
       // Si la requête a réussi, envoyez l'image résultante
       
       let stickerMess = new Sticker(response.data.result, {
-        pack: FLASH-MD,
+        pack: MBW-MD,
         type: StickerTypes.CROPPED,
         categories: ["🤩", "🎉"],
         id: "12345",
