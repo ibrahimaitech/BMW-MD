23301155dd6f90ed333504ac6eea358e7b263f2e
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-03 16:19:05 +0300

    Update repo.js

--- commandes/repo.js
+++ commandes/repo.js
@@ -4,7 +4,6 @@ const { france } = require("../framework/france");
 france({ nomCom: "repo", reaction: "🚗", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = '𝗢𝘇𝗮𝗮𝗵👊 𝗧𝗵𝗶𝘀 𝗶𝘀 *𝗕𝗠𝗪-𝗠𝗗*  𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗯𝘆 *𝗜𝗕𝗥𝗔𝗛𝗜𝗠 𝗔𝗗𝗔𝗠𝗦*🥷 𝗣𝗹𝗲𝗮𝘀𝗲 𝗚𝗶𝘃𝗲 𝗮 𝗦𝘁𝗮𝗿 ✨ 𝗢𝗻 𝗧𝗵𝗲 𝗥𝗲𝗽𝗼 𝗔𝗻𝗱 𝗙𝗼𝗿𝗸 𝗜𝘁 🚗 \n\n ' + "║❒ *𝗥𝗲𝗽𝗼*https://github.com/ibrahimaitech/IBRAHIM-AI-10.10.*";
-    
     let d = ' ║❒ *𝗬𝗼𝘂𝗧𝘂𝗯𝗲*https://www.youtube.com/@ibrahimmdgpt*"';
     let b = ' ║❒ *𝗜𝗻𝘀𝘁𝗮𝗴𝗿𝗮𝗺*https://www.instagram.com/ibrahim_tech_official*";
     let varmess = z + d;
