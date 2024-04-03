e03b57eeef1cac487e8bed99dcbe1a80d3d2995b
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-03 14:26:45 +0300

    ping

--- commandes/ping2.js
+++ commandes/ping2.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
 france({ nomCom: "ping", reaction: "💯", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝐏𝐈𝐍𝐆𝐈𝐍𝐆 𝐅𝐑𝐎𝐌⌛*𝗜𝗕𝗥𝗔𝗛𝗜𝗠-𝗧𝗘𝗖𝗛* \n\n ' + "LOADING⏳..✨✨✨..100℅✅";
-    let d = '𝗣𝗢𝗡𝗚:1090𝗠𝗦⏳*';
+    let z = '𝐏𝐈𝐍𝐆𝐈𝐍𝐆 𝐅𝐑𝐎𝐌⌛*𝗜𝗕𝗥𝗔𝗛𝗜𝗠-𝗧𝗘𝗖𝗛* \n\n ' + "*⌛ _Charging..._*
+*▰▰▰▱▱▱▱▱*✅";
+    let d = '𝗣𝗢𝗡𝗚:1356𝗠𝗦*';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/e3d2edb3167a59ba625d7.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
