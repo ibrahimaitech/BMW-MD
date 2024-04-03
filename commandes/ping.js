7db68c947201487ea94d272ac1fd8afd9f333604
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-03 17:41:19 +0300

    Add files via upload

---
+++ commandes/ping.js
@@ -0,0 +1,22 @@
+const { france } = require("../framework/france");
+const moment = require("moment-timezone");
+const { default: axios } = require('axios');
+//const conf = require('../set');
+
+
+france({ nomCom: 'ping',
+    desc: 'To check ping',
+    Categorie: 'General',
+    reaction: '🚗', 
+    fromMe: 'true', 
+
+       
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+    const { start} = new Date().getTime()
+    return repondre('*Pong!!*\n ```' + 8760 + '``` *arg*') 
+    const { end } = new Date().getTime()
+    await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
+  }
+)
