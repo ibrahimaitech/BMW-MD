3ef3deb17b98fd9d341f626f28facaa3854cafb8
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-05 18:11:16 +0300

    BMW-MD

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -8,7 +8,7 @@ france({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, z
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExample : ${prefixe}fancy 10 Flash-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('FLASH-MD', fancy));
+            return await repondre(`\nExample : ${prefixe}fancy 10 Bmw-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('FLASH-MD', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
