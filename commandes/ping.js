b14e4e0d22022810c5dc58b9b40876e74e73dcd3
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-06 23:22:25 +0300

    😂😂😂

---
+++ commandes/ping.js
@@ -0,0 +1,22 @@
+const { france } = require("../framework/france");
+const moment = require("moment-timezone");
+const { default: axios } = require('axios');
+//const conf = require('../set');
+
+
+france({ nomCom: 'ping',
+    desc: 'To check ping',
+    Categorie: 'General',
+    reaction: '🚕', 
+    fromMe: 'true', 
+
+       
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+    const { start} = new Date().getTime()
+    return repondre('*Pong▰▰▰▱▱▱▱▱*\n ```' + 8754 + '``` *ms*') 
+    const { end } = new Date().getTime()
+    await zok.sendMessage('*Pong▰▰▰▱▱▱▱▱*\n ```' + (end - start) + '``` *ms*')
+  }
+)
