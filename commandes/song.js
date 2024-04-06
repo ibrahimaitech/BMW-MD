1ded0ebf009db68b4d11126cf355d471c9f0a9be
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-06 18:31:12 +0300

    BMW-MD🚘

--- commandes/song.js
+++ commandes/song.js
@@ -8,7 +8,7 @@ const yts1 = require("youtube-yts");
 //var fs =require("fs-extra")
 
 france({
-  nomCom: "sonh",
+  nomCom: "song",
   categorie: "Search",
   reaction: "🎸"
 }, async (origineMessage, zk, commandeOptions) => {
@@ -75,70 +75,6 @@ _*DOWNLOADING...*_\n\n`
   } catch (error) {
     console.error('Erreur lors de la recherche ou du téléchargement de la vidéo :', error);
     
-    repondre('Une erreur est survenue lors de la recherche ou du téléchargement de la vidéo.');
-  }
-});
-
-  
-
-france({
-  nomCom: "video2",
-  categorie: "Search",
-  reaction: "📽"
-}, async (origineMessage, zk, commandeOptions) => {
-  const { arg, ms, repondre } = commandeOptions;
-
-  if (!arg[0]) {
-    repondre("insert video name");
-    return;
-  }
-
-  const topo = arg.join(" ");
-  try {
-    const search = await yts(topo);
-    const videos = search.videos;
-
-    if (videos && videos.length > 0 && videos[0]) {
-      const Element = videos[0];
-
-      let InfoMess = {
-        image: { url: videos[0].thumbnail },
-        caption: `*Video name :* _${Element.title}_
-*Time :* _${Element.timestamp}_
-*Url :* _${Element.url}_
-_*DOWNLOADING...*_\n\n`
-      };
-
-      zk.sendMessage(origineMessage, InfoMess, { quoted: ms });
-
-      // Obtenir les informations de la vidéo à partir du lien YouTube
-      const videoInfo = await ytdl.getInfo(Element.url);
-      // Format vidéo avec la meilleure qualité disponible
-      const format = ytdl.chooseFormat(videoInfo.formats, { quality: '18' });
-      // Télécharger la vidéo
-      const videoStream = ytdl.downloadFromInfo(videoInfo, { format });
-
-      // Nom du fichier local pour sauvegarder la vidéo
-      const filename = 'video.mp4';
-
-      // Écrire le flux vidéo dans un fichier local
-      const fileStream = fs.createWriteStream(filename);
-      videoStream.pipe(fileStream);
-
-      fileStream.on('finish', () => {
-        // Envoi du fichier vidéo en utilisant l'URL du fichier local
-        zk.sendMessage(origineMessage, { video: { url :"./video.mp4"} , caption: "*BMW-MD*", gifPlayback: false }, { quoted: ms });
-      });
-
-      fileStream.on('error', (error) => {
-        console.error('Erreur lors de l\'écriture du fichier vidéo :', error);
-        repondre('Une erreur est survenue lors de l\'écriture du fichier vidéo.');
-      });
-    } else {
-      repondre('No video found');
-    }
-  } catch (error) {
-    console.error('Erreur lors de la recherche ou du téléchargement de la vidéo :', error);
     repondre('Une erreur est survenue lors de la recherche ou du téléchargement de la vidéo.');
   }
 });
