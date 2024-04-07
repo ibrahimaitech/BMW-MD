136783a27842a5e86b5ab792517c71d2cafc4b8e
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-07 10:19:21 +0300

    BMW-MD

---
+++ commandes/wallpaper.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { france } = require("../framework/france");
+france({ nomCom: "script", reaction: "🎞", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = '*Random Wallpaper \n\n ' + "Download it and set it to your wallpaper *BMW-MD*";
+    let d = 'MADE BY *IBRAHIM-ADAMS*';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/a99074213b1bf1351a9d8.jpg,https://telegra.ph/file/c90fdab5f133a946d6f4f.jpg,https://telegra.ph/file/97d3d23af7aff33934aea.jpg,https://telegra.ph/file/480533ebef1d58e17f270.jpg,https://telegra.ph/file/45e33d43711a7c041853f.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+/*module.exports.commande = () => {
+  var nomCom = ["test","t"]
+  var reaction="☺️"
+  return { nomCom, execute,reaction }
+};
+
+async function  execute  (origineMessage,zok) {
+  console.log("Commande saisie !!!s")
+   let z ='Salut je m\'appelle *BMW-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *BMW-MD-REPO*'
+      let varmess=z+d
+      var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
+await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
+}  */ 
