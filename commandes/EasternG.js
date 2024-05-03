ac08e065c9d56f4775211bd20ac95cc92a12ee0a
𝔼𝕒𝕤𝕥𝕖𝕣𝕟 𝔾𝕦𝕣𝕦 <168541653+EasternG@users.noreply.github.com>
2024-05-03 04:10:33 +0300

    EasternG.js
    Eastern GURU

--- commandes/EasternG.js
+++ commandes/EasternG.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
-france({ nomCom: "ibrahim", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+france({ nomCom: "easternguru", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Yes you called me whats up Ok anyway support me by following my channel and subscribing on my youtube channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
-    let d = '                                        ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@ibrahimaitech';
+    let z = 'Yes you called me whats up😁!! Anyway, support me by following my channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VaZChvyLY6d8PnO4x61B";
+    let d = 'Tell me a story Today!;
     let varmess = z + d;
     var mp4 = 'https://telegra.ph/file/dec033e457813cd2ff2e3.mp4';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
