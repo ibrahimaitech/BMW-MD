c714386156de7b3ebb0cf008e3fd9a39125e45a1
Toputech <168991681+Toputech@users.noreply.github.com>
2024-05-11 19:30:44 +0300

    Update Ibrahim.js
    "use strict";
    Object.defineProperty(exports, "__esModule", { value: true });
    const { france } = require("../framework/france");
    france({ nomCom: "ibrahim", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
        console.log("Commande saisie !!!s");
        let z = 'Yes you called me whats up Ok anyway support me by following my channel and subscribing on my youtube channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
        let d = '                                        ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@ibrahimaitech';
        let varmess = z + d;
        var mp4 = 'https://telegra.ph/file/dec033e457813cd2ff2e3.mp4';
        await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
        //console.log("montest")
    });
    console.log("mon test");
    /*module.exports.commande = () => {
      var nomCom = ["test","t"]
      var reaction="☺️"
      return { nomCom, execute,reaction }
    };
    
    async function  execute  (origineMessage,zok) {
      console.log("Commande saisie !!!s")
       let z ='Salut je m\'appelle *BMW-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
          let d =' developpé par *BMW-MD-REPO*'
          let varmess=z+d
          var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
    await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
    }  */

--- commandes/Ibrahim.js
+++ commandes/Ibrahim.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
 france({ nomCom: "ibrahim", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Yes you called me whats up Ok anyway support me by following my channel and subscribing on my youtube channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VaeRrcnADTOKzivM0S1r";
+    let z = 'Yes you called me whats up Ok anyway support me by following my channel and subscribing on my youtube channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
     let d = '                                        ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@ibrahimaitech';
     let varmess = z + d;
     var mp4 = 'https://telegra.ph/file/dec033e457813cd2ff2e3.mp4';
