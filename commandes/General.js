b58338c06f2575540f02e287cc9739943e99a289
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2024-05-15 09:42:06 +0300

    Bmw md v2 🚖

--- commandes/General.js
+++ commandes/General.js
@@ -1,1 +1,106 @@
-const _0x555df1=_0x2c29;function _0x2c29(_0x2513f8,_0x447bc8){const _0x5bcb00=_0x5bcb();return _0x2c29=function(_0x2c2965,_0x518b89){_0x2c2965=_0x2c2965-0x1cb;let _0x19a8ce=_0x5bcb00[_0x2c2965];return _0x19a8ce;},_0x2c29(_0x2513f8,_0x447bc8);}function _0x5bcb(){const _0x508203=['ORG:undefined;\x0a','----------------\x0a•\x20','5912627STWthz','BEGIN:VCARD\x0a','8QYeAbf','General','VERSION:3.0\x0a','nom','280452LeYQTX','support','OWNER_NAME','../framework/france','../bdd/sudo','406073POrMef','2mfooZH','link\x20error','developer','164624fcZBFT','concat','🥵🥵\x20Menu\x20erreur\x20','254710772666','owner','3055620XNeabI','sendMessage','log','END:VCARD','match','4189550HaaQfq','6PkEeNY','3180915sXEEgs','\x20:\x20https://wa.me/','TEL;type=CELL;type=VOICE;waid=','replace','NUMERO_OWNER','FN:'];_0x5bcb=function(){return _0x508203;};return _0x5bcb();}(function(_0x2801ec,_0x42d969){const _0x14c5a3=_0x2c29,_0x331689=_0x2801ec();while(!![]){try{const _0x57ef53=parseInt(_0x14c5a3(0x1d7))/0x1+parseInt(_0x14c5a3(0x1d8))/0x2*(-parseInt(_0x14c5a3(0x1d2))/0x3)+parseInt(_0x14c5a3(0x1db))/0x4+-parseInt(_0x14c5a3(0x1e0))/0x5+parseInt(_0x14c5a3(0x1e6))/0x6*(parseInt(_0x14c5a3(0x1cc))/0x7)+-parseInt(_0x14c5a3(0x1ce))/0x8*(-parseInt(_0x14c5a3(0x1e7))/0x9)+-parseInt(_0x14c5a3(0x1e5))/0xa;if(_0x57ef53===_0x42d969)break;else _0x331689['push'](_0x331689['shift']());}catch(_0x476053){_0x331689['push'](_0x331689['shift']());}}}(_0x5bcb,0x7f622));const {france}=require(_0x555df1(0x1d5)),{getAllSudoNumbers,isSudoTableNotEmpty}=require(_0x555df1(0x1d6)),conf=require('../set');france({'nomCom':_0x555df1(0x1df),'categorie':_0x555df1(0x1cf),'reaction':'💞'},async(_0x63582c,_0x26a5e1,_0x5a8195)=>{const _0x2dc027=_0x555df1,{ms:_0x5b3c02,mybotpic:_0x56ba43}=_0x5a8195,_0x1c7791=await isSudoTableNotEmpty();if(_0x1c7791){let _0x508737='*My\x20Super-User*\x0a\x0a\x20\x20\x20\x20\x20*Owner\x20Number*\x0a\x20:\x0a-\x20🌟\x20@'+conf['NUMERO_OWNER']+'\x0a\x0a------\x20*other\x20sudos*\x20-----\x0a',_0x1b838b=await getAllSudoNumbers();for(const _0x23edcb of _0x1b838b){if(_0x23edcb)sudonumero=_0x23edcb['replace'](/[^0-9]/g,''),_0x508737+='-\x20💼\x20@'+sudonumero+'\x0a';else return;}const _0x1af635=conf[_0x2dc027(0x1eb)][_0x2dc027(0x1ea)](/[^0-9]/g)+'@s.whatsapp.net',_0x13ce69=_0x1b838b[_0x2dc027(0x1dc)]([_0x1af635]);console['log'](_0x1b838b),console[_0x2dc027(0x1e2)](_0x13ce69),_0x26a5e1[_0x2dc027(0x1e1)](_0x63582c,{'image':{'url':_0x56ba43()},'caption':_0x508737,'mentions':_0x13ce69});}else{const _0x5813eb=_0x2dc027(0x1cd)+_0x2dc027(0x1d0)+_0x2dc027(0x1ec)+conf[_0x2dc027(0x1d4)]+'\x0a'+_0x2dc027(0x1ed)+_0x2dc027(0x1e9)+conf[_0x2dc027(0x1eb)]+':+'+conf[_0x2dc027(0x1eb)]+'\x0a'+_0x2dc027(0x1e3);_0x26a5e1[_0x2dc027(0x1e1)](_0x63582c,{'contacts':{'displayName':conf[_0x2dc027(0x1d4)],'contacts':[{'vcard':_0x5813eb}]}},{'quoted':_0x5b3c02});}}),france({'nomCom':_0x555df1(0x1da),'categorie':'General','reaction':'💞'},async(_0x149ba7,_0x2e0ac0,_0x18f95a)=>{const _0xa0e096=_0x555df1,{ms:_0x4ce664,mybotpic:_0x40d93b}=_0x18f95a,_0xdcef24=[{'nom':'King\x20Ibrahim','numero':_0xa0e096(0x1de)},{'nom':'᚛kingibrahim᚜','numero':_0xa0e096(0x1de)}];let _0xfd808='👊\x20*Welcome\x20to\x20BMW-MD!*\x20here\x20is\x20the\x20developer\x20numbers:\x0a\x0a';for(const _0x36ccca of _0xdcef24){_0xfd808+=_0xa0e096(0x1cb)+_0x36ccca[_0xa0e096(0x1d1)]+_0xa0e096(0x1e8)+_0x36ccca['numero']+'\x0a';}var _0x23aac1=_0x40d93b();if(_0x23aac1[_0xa0e096(0x1e4)](/\.(mp4|gif)$/i))try{_0x2e0ac0[_0xa0e096(0x1e1)](_0x149ba7,{'video':{'url':_0x23aac1},'caption':_0xfd808},{'quoted':_0x4ce664});}catch(_0xfb3fc7){console[_0xa0e096(0x1e2)](_0xa0e096(0x1dd)+_0xfb3fc7),repondre('🥵🥵\x20Menu\x20erreur\x20'+_0xfb3fc7);}else{if(_0x23aac1['match'](/\.(jpeg|png|jpg)$/i))try{_0x2e0ac0[_0xa0e096(0x1e1)](_0x149ba7,{'image':{'url':_0x23aac1},'caption':_0xfd808},{'quoted':_0x4ce664});}catch(_0x138074){console[_0xa0e096(0x1e2)]('🥵🥵\x20Menu\x20erreur\x20'+_0x138074),repondre(_0xa0e096(0x1dd)+_0x138074);}else repondre(_0x23aac1),repondre(_0xa0e096(0x1d9));}}),france({'nomCom':_0x555df1(0x1d3),'categorie':_0x555df1(0x1cf)},async(_0x249f25,_0x17466d,_0x45e262)=>{const _0x562b6e=_0x555df1,{ms:_0x510f2d,repondre:_0x5ec669,auteurMessage:_0x2893b5}=_0x45e262;_0x5ec669('Support\x20the\x20developer\x20by\x20subscribing\x20on\x20youtube\x20\x20'),await _0x17466d[_0x562b6e(0x1e1)](_0x2893b5,{'text':'https://www.youtube.com/@ibrahimaitech'},{'quoted':_0x510f2d});});
+
+
+
+
+const { france } = require("../framework/france");
+const {getAllSudoNumbers,isSudoTableNotEmpty} = require("../bdd/sudo")
+const conf = require("../set");
+
+france({ nomCom: "owner", categorie: "General", reaction: "🧔" }, async (dest, zk, commandeOptions) => {
+    const { ms , mybotpic } = commandeOptions;
+    
+  const thsudo = await isSudoTableNotEmpty()
+
+  if (thsudo) {
+     let msg = `*My Super-User*\n
+     *Owner Number*\n :
+- 🌟 @${conf.NUMERO_OWNER}
+
+------ *other sudos* -----\n`
+     
+ let sudos = await getAllSudoNumbers()
+
+   for ( const sudo of sudos) {
+    if (sudo) { // Vérification plus stricte pour éliminer les valeurs vides ou indéfinies
+      sudonumero = sudo.replace(/[^0-9]/g, '');
+      msg += `- 💼 @${sudonumero}\n`;
+    } else {return}
+
+   }   const ownerjid = conf.NUMERO_OWNER.replace(/[^0-9]/g) + "@s.whatsapp.net";
+   const mentionedJid = sudos.concat([ownerjid])
+   console.log(sudos);
+   console.log(mentionedJid)
+      zk.sendMessage(
+        dest,
+        {
+          image : { url : mybotpic() },
+          caption : msg,
+          mentions : mentionedJid
+        }
+      )
+  } else {
+    const vcard =
+        'BEGIN:VCARD\n' + // metadata of the contact card
+        'VERSION:3.0\n' +
+        'FN:' + conf.OWNER_NAME + '\n' + // full name
+        'ORG:undefined;\n' + // the organization of the contact
+        'TEL;type=CELL;type=VOICE;waid=' + conf.NUMERO_OWNER + ':+' + conf.NUMERO_OWNER + '\n' + // WhatsApp ID + phone number
+        'END:VCARD';
+    zk.sendMessage(dest, {
+        contacts: {
+            displayName: conf.OWNER_NAME,
+            contacts: [{ vcard }],
+        },
+    },{quoted:ms});
+  }
+});
+
+france({ nomCom: "developer", categorie: "General", reaction: "🚘" }, async (dest, zk, commandeOptions) => {
+    const { ms, mybotpic } = commandeOptions;
+
+    const devs = [
+      { nom: "France King", numero: "254710772666" },
+      { nom: "᚛Ibrahimadams᚜", numero: "254710772666" },
+      // Ajoute d'autres développeurs ici avec leur nom et numéro
+    ];
+
+    let message = "Oyaah 👊\n *Welcome to BMW-Md!*\n\n*💠━━💠CREATOR INFO💠━━💠*\n\n\n *◇NAME: _Ibrahim Adams_*\n*◇AGE: 20*\n*◇LOCATION: _On Earth_*\n*◇BELIEVER: YES ✅*\n*◇FRIENDLY: YES ✅*\n*◇EGOISTIC: _YEEES ✅_*\n*◇FAV MOVIE: _ONE EYE_*\n*◇INSTAGRAM: https://instagram.com/Ibrahim_tech_official*\n*◇Twitter: https://twitter.com/Ibrahim_adams*\n\n______________________________\n\n*🚖KEEP USING BMW-MD🚖*\n\n\n_The following are my contacts_:\n:";
+    for (const dev of devs) {
+      message += `----------------\n• ${dev.nom} : https://wa.me/${dev.numero}\n`;
+    }
+   
+  var lien = mybotpic()
+    if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption: message }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:message }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    repondre(lien)
+    repondre("link error");
+    
+}
+});
+
+france({ nomCom: "support", categorie: "General" }, async (dest, zk, commandeOptions) => {
+  const { ms, repondre, auteurMessage, } = commandeOptions; 
+ 
+  repondre("look on pm sir ")
+  await zk.sendMessage(auteurMessage,{text : `https://wa.me/254757835036`},{quoted :ms})
+
+})
+ 
