37ab18a26bf522f54d7cd7b0b9f90e291440a979
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2024-05-20 21:43:10 +0300

    BMW X5 🥏

---
+++ commandes/lubao.js
@@ -0,0 +1,38 @@
+const {france}=require("../framework/france")
+
+
+
+
+
+
+
+france({nomCom:"beb",categorie:"Mods",reaction:"❤"},async(dest,z,com)=>{
+
+
+  let pingMsg = await conn.sendMessage(m.chat, {text: 'Pinging...'})
+
+  let timestamp = speed()
+
+  await exec('neofetch --stdout', async (error, stdout) => {
+
+    let latency = (speed() - timestamp).toFixed(4)
+
+    await conn.relayMessage(m.chat, {
+      protocolMessage: {
+        key: pingMsg.key,
+        type: 14,
+        editedMessage: {
+          conversation: `Pong! Latency: ${latency} ms` 
+        }
+      }
+    }, {})
+
+  })
+
+}
+
+handler.help = ['ping']
+handler.tags = ['main']
+handler.command = ['ping', 'speed'] 
+
+export default handler
