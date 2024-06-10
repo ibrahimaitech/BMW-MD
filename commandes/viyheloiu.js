85066cc0972be85adb797bfaab5eb713cd5a01b8
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2024-06-10 14:35:01 +0300

    BMW X5 🥏

---
+++ commandes/viyheloiu.js
@@ -0,0 +1,93 @@
+const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/france");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+
+zokou({ nomCom: "menu3", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../framework//france");
+    var coms = {};
+    var mode = "public";
+    
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
+
+
+    
+ cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault('EAT');
+
+// Créer une date et une heure en EAT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+  let infoMsg =  `
+╭────《🄱🄼🅆-🄼🄳》────☣
+|🔱 *User* : ${s.OWNER_NAME}
+|🔱 *Mode* : ${mode}
+|🔱 *Commands* : ${cm.length} 
+|🔱 *Time* : ${temps} 
+|🔱 *Ram* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+|🔰 🄸🄱🅁🄰🄷🄸🄼 🄰🄳🄰🄼🅂
+╰─────✨✨✨✨✨─────☣ \n\n`;
+ 
+    let menuMsg=`  
+  ┆┆┆┆┆┆┆┆┆┆┆┆┆┆┆┆┆┆┆┆┆┆
+✨➽➽➽➽➽➽➽➽➽➽➽➽➽➽↰✨
+`;
+
+    for (const cat in coms) {
+        menuMsg += `*╭────☣* *${cat}* *☣⊷*`;
+        for (const cmd of coms[cat]) {
+            menuMsg += `  
+*|* ${cmd}`;
+        }
+        menuMsg += `
+*↪═════════════☣* \n`
+    }
+
+    menuMsg += `
+◇            ◇
+*————— ✨ —————*
+
+  *ᵏⁱⁿᵍ ⁱᵇʳᵃʰⁱᵐ ᵗᵉᶜʰ 2024🎇*                                         
+*↪═════════════☣*
+`;
+
+   var lien = mybotpic();
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Zokou-MD*, développé par Djalega++" , gifPlayback : true }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "*Ibrahim-tech*" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(infoMsg + menuMsg);
+    
+}
+
+});
